b22cd1661f656b454e911166d251729f5e8bc37a
Archer <zhongchuqiao1201@gmail.com>
2023-11-24 16:18:54 -0800

    php

--- genshinGuideTemp.sql
+++ genshinGuideTemp.sql
@@ -12,38 +12,44 @@ CREATE TABLE IF NOT EXISTS users(
     password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS weaponTypes(
+    weaponTypeID INT AUTO_INCREMENT PRIMARY KEY,
+    weaponTypeName VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS characters(
     characterID INT AUTO_INCREMENT PRIMARY KEY,
     -- 查询数据
-    characterName VARCHAR(50) NOT NULL,
-    weaponType VARCHAR(20) NOT NULL,
+    name VARCHAR(50) NOT NULL,
     elementType VARCHAR(20) NOT NULL,
     region VARCHAR(20) NOT NULL,
     rarity VARCHAR(20) NOT NULL,
+    weaponTypeID INT,
+    FOREIGN KEY (weaponTypeID) REFERENCES weaponTypes(weaponTypeID),
     -- url数据
-    characterImage VARCHAR(255),
-    characterIcon VARCHAR(255),
-    characterDescription VARCHAR(255)
+    image VARCHAR(255),
+    description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS weapons(
     weaponID INT AUTO_INCREMENT PRIMARY KEY,
     -- 查询数据
-    weaponName VARCHAR(50) NOT NULL,
-    weaponType VARCHAR(20) NOT NULL,
+    name VARCHAR(50) NOT NULL,
     weaponRarity VARCHAR(20) NOT NULL,
+    weaponTypeID INT,
+    FOREIGN KEY (weaponTypeID) REFERENCES weaponTypes(weaponTypeID),
     -- url数据
-    weaponImage VARCHAR(255),
-    weaponDescription VARCHAR(255)
+    image VARCHAR(255),
+    description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS artifacts(
     artifactID INT AUTO_INCREMENT PRIMARY KEY,
     -- 查询数据
-    artifactName VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
     -- url数据
-    artifactImage VARCHAR(255),
-    artifactDescription VARCHAR(255)
+    image VARCHAR(255),
+    description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS guides(
@@ -69,10 +75,22 @@ CREATE TABLE IF NOT EXISTS guides(
     FOREIGN KEY (artifactID_2) REFERENCES artifacts(artifactID)
 );
 
-CREATE TABLE IF NOT EXISTS user_favoriate(
+CREATE TABLE IF NOT EXISTS user_favorite(
     -- 查询数据
     userID INT,
     FOREIGN KEY (userID) REFERENCES users(userID),
     guideID INT,
     FOREIGN KEY (guideID) REFERENCES guides(guideID)
 );
+
+CREATE TABLE IF NOT EXISTS comments(
+    commentID INT AUTO_INCREMENT PRIMARY KEY,
+    postDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    text TEXT,
+    likes INT DEFAULT 0,
+    -- 查询数据
+    userID INT,
+    FOREIGN KEY (userID) REFERENCES users(userID),
+    guideID INT,
+    FOREIGN KEY (guideID) REFERENCES guides(guideID)
+)
--- php/charactersGallery.php
+++ php/charactersGallery.php
@@ -1,2 +1,46 @@
 <?php
+
+$dbserver = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "genshinGuide";
+
+$db = new mysqli($dbserver, $dbuser, $dbpass, $dbname);
+if ($db->connect_errno) {
+    echo "Database connection error: " . $db->connect_error;
+    exit();
+}
+
 echo "<h1>Characters</h1>";
+
+echo "<div>";
+$query_str = 'SELECT * FROM characters';
+$res = $db->query($query_str);
+if ($res === false) {
+    echo "Query error: " . $db->error;
+    exit();
+}
+while ($row = $res->fetch_assoc()) {
+    loadInfoCard("characters", $row['name'], $row['image']);
+}
+
+echo "</div>";
+
+$res->free_result();
+$db->close();
+
+function loadInfoCard($table, $name, $image){
+    $resRoot = null;
+    if ($table == "characters") $resRoot = "../res/CharacterImages/";
+    else if ($table == "weapons") $resRoot = "../res/WeaponImages/";
+    else if ($table == "artifacts") $resRoot = "../res/ArtifactImages/";
+
+    echo "<div>";
+    if ($resRoot == null){
+        echo $table . " is invalid";
+        return;
+    }
+    echo "<img src='" . $resRoot . $image . "'>";
+    echo "<p>$name</p>";
+    echo "</div>";
+}
